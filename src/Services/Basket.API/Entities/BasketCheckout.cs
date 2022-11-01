bb5296382cebb5b45c8b92d9744267212f4b483c
Minh Tri Nguyen <minhtripro2903@gmail.com>
2022-11-01 22:05:38 +0700

    develop BasketAPI

---
+++ src/Services/Basket.API/Entities/BasketCheckout.cs
@@ -0,0 +1,8 @@
+﻿public class BasketCheckout
+{
+    public string UserName { get; set; }
+    public decimal TotalPrice { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+}
