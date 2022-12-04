74cc1fa16e85c3cab2c560fbd604676b1870a37e
Minh Tri Nguyen <minhtripro2903@gmail.com>
2022-12-04 12:56:11 +0700

    Intergrate OrderAPI

--- src/BuildingBlocks/Contracts/Common/Interfaces/IRepositoryBase.cs
+++ src/BuildingBlocks/Contracts/Common/Interfaces/IRepositoryBase.cs
@@ -5,12 +5,11 @@ using Microsoft.EntityFrameworkCore.Storage;
 
 namespace Contracts.Common.Interfaces;
 
-public interface IRepositoryQueryBase<T, K, TContext> where T : EntityBase<K> where TContext : DbContext
+public interface IRepositoryQueryBase<T, in K> where T : EntityBase<K>
 {
     IQueryable<T> FindAll(bool trackChanges = false);
     IQueryable<T> FindAll(bool trackChanges = false, params Expression<Func<T, object>>[] includeProperties);
     IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges = false);
-
     IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges = false,
         params Expression<Func<T, object>>[] includeProperties);
 
@@ -18,9 +17,14 @@ public interface IRepositoryQueryBase<T, K, TContext> where T : EntityBase<K> wh
     Task<T?> GetByIdAsync(K id, params Expression<Func<T, object>>[] includeProperties);
 }
 
-public interface IRepositoryBase<T, K, TContext> : IRepositoryQueryBase<T, K, TContext>
-    where T : EntityBase<K>
+public interface IRepositoryQueryBase<T, K, TContext> : IRepositoryQueryBase<T, K> where T : EntityBase<K>
     where TContext : DbContext
+{
+
+}
+
+public interface IRepositoryBase<T, K> : IRepositoryQueryBase<T, K>
+    where T : EntityBase<K>
 {
     Task<K> CreateAsync(T entity);
     Task<IList<K>> CreateListAsync(IEnumerable<T> entities);
@@ -29,8 +33,13 @@ public interface IRepositoryBase<T, K, TContext> : IRepositoryQueryBase<T, K, TC
     Task DeleteAsync(T entity);
     Task DeleteListAsync(IEnumerable<T> entities);
     Task<int> SaveChangesAsync();
-
     Task<IDbContextTransaction> BeginTransactionAsync();
     Task EndTransactionAsync();
     Task RollbackTransactionAsync();
+}
+
+public interface IRepositoryBase<T, K, TContext> : IRepositoryBase<T, K>
+    where T : EntityBase<K>
+    where TContext : DbContext
+{
 }
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Entities;
+using Ordering.Domain.Enums;
+using Serilog;
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        private readonly ILogger _logger;
+        private readonly OrderContext _orderContext;
+        public OrderContextSeed(ILogger logger, OrderContext orderContext)
+        {
+            _logger = logger;
+            _orderContext = orderContext;
+        }
+
+        public async Task InitialiseAsync()
+        {
+            try
+            {
+                if (_orderContext.Database.IsSqlServer())
+                {
+                    await _orderContext.Database.MigrateAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "An error occurred while initialising the database.");
+                throw;
+            }
+        }
+
+        public async Task TrySeedAsync()
+        {
+            if (!_orderContext.Orders.Any())
+            {
+                await _orderContext.Orders.AddRangeAsync(
+                    new Order
+                    {
+                        UserName = "customer1",
+                        FirstName = "customer1",
+                        LastName = "customer",
+                        EmailAddress = "customer1@local.com",
+                        ShippingAddress = "Wollongong",
+                        InvoiceAddress = "Australia",
+                        TotalPrice = 250
+                    },
+                    new Order
+                    {
+                        UserName = "customer2",
+                        FirstName = "customer2",
+                        LastName = "customer",
+                        EmailAddress = "customer2@local.com",
+                        ShippingAddress = "Wollongong",
+                        InvoiceAddress = "Australia",
+                        TotalPrice = 250
+                    });
+            }
+        }
+
+
+        public async Task SeedAsync()
+        {
+            try
+            {
+                await TrySeedAsync();
+                await _orderContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, "An error occurred while seeding the database.");
+                throw;
+            }
+        }
+    }
+}
