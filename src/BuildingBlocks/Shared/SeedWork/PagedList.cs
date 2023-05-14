06c6960f658c55cc637ec91bcaabcf208775e550
Minh Tri Nguyen <minhtripro2903@gmail.com>
2023-05-14 14:48:30 +0700

    Xây dựng Service Layer với CRUD methods và abstraction pagination

--- src/BuildingBlocks/Shared/SeedWork/PagedList.cs
+++ src/BuildingBlocks/Shared/SeedWork/PagedList.cs
@@ -1,4 +1,6 @@
-﻿namespace Shared.SeedWork;
+﻿using MongoDB.Driver;
+
+namespace Shared.SeedWork;
 
 public class PagedList<T> : List<T>
 {
@@ -19,5 +21,16 @@ public class PagedList<T> : List<T>
     public MetaData GetMetaData()
     {
         return _metaData;
+    }
+
+    public static async Task<PagedList<T>> ToPagedList(IMongoCollection<T> source, FilterDefinition<T> filter, int pageIndex, int pageSize)
+    {
+        var count = await source.Find(filter).CountDocumentsAsync();
+        var items = await source.Find(filter)
+            .Skip((pageIndex - 1) * pageSize)
+            .Limit(pageSize)
+            .ToListAsync();
+
+        return new PagedList<T>(items, count, pageIndex, pageSize);
     }
 }
